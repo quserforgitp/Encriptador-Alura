247c138341d64bd4a24c8829941a4552d7f075a8
Helios <adealumnonegligas@gmail.com>
2022-09-13 15:17:21 -0500

    completamente funcional

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
+const input = document.querySelector("#input-text");
+const output = document.querySelector("#output-text");
+const btnCopiar = document.querySelector(".boton-copiar");
+const avisoNoTexto = document.querySelector(".p-mensaje-no-encontrado");
+const avisoIngresaTexto = document.querySelector(".p-ingresa-texto");
 
+window.onload = init();
 
-function encriptarTexto(stringEncriptada) {
+function init() {
+    input.value = "";
+    output.value = "";
+    mostrarElementos();
+}
+
+function btnEncriptar() {
+
+    if (input.value == "") {
+        btnCopiar.style.display = "none";
+        output.value = "";
+        mostrarElementos();
+    } else {
+        const textoProcesado = encriptar(input.value);
+        ocultarElementos();
+        output.value = textoProcesado;
+        input.value = "";
+        btnCopiar.style.display = "block";
+    }
+}
+
+function btnDesencriptar() {
+    
+    if (input.value == "") {
+        btnCopiar.style.display = "none";
+        output.value = "";
+        mostrarElementos();
+    } else {
+        const textoProcesado = desencriptar(input.value);
+        ocultarElementos();
+        output.value = textoProcesado;
+        input.value = "";
+        btnCopiar.style.display = "block";
+    }
+}
+
+function encriptar(stringEncriptada) {
 
     let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o","ober"], ["u","ufat"]];
 
@@ -15,7 +57,7 @@ function encriptarTexto(stringEncriptada) {
 }
 
 
-function desecriptarTexto(stringDesencriptada){
+function desencriptar(stringDesencriptada){
     let matrizCodigo = [ ["e", "enter"], ["i", "imes"], ["a", "ai"], ["o","ober"], ["u","ufat"]];
     stringDesencriptada = stringDesencriptada.toLowerCase();
 
@@ -27,3 +69,25 @@ function desecriptarTexto(stringDesencriptada){
 
     return stringDesencriptada;
 }
+
+function ocultarElementos() {
+    output.style.backgroundImage = "none";
+    avisoNoTexto.style.display = "none";
+    avisoIngresaTexto.style.display = "none";
+}
+
+function mostrarElementos() {
+    output.style.backgroundImage = "url(src/img/muneco.svg)";
+    avisoNoTexto.style.display = "block";
+    avisoIngresaTexto.style.display = "block";
+}
+
+function copiar() {
+    
+    output.select();
+    navigator.clipboard.writeText(output.value)
+    output.value = "";
+    mostrarElementos();
+    alert("Texto Copiado")
+}
+
