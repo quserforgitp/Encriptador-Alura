45c14999b4a60f7007680783f823c034904e1318
Helios <adealumnonegligas@gmail.com>
2022-09-13 11:38:12 -0500

    añadidas funciones y sus referencias en sus respectivos botones

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+function encriptarTexto() {
+    alert("function encriptarTexo");
+}
+
+function desencriptarTexto() {
+    alert("function desencriptarTexto");
+}
+
+function copiarTexto() {
+    alert("function copiarTexto");
+}
+
+function ocultarElementos() {
+    alert("function ocultarElementos");
+}
+
+function mostrarElementos() {
+    alert("function mostrarElementos");
+}
+
+function borrarTexto() {
+    alert("function borrarTexto");
+}
